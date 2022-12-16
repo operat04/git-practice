6880ddb81287d8ebcf89ec0b90acbddad739f9e4
operat04 <operat04@naver.com>
2022-12-16 22:38:30 +0900

    약간 수정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { createStore } from "@reduxjs/toolkit";
+import { Provider, useDispatch, useSelector } from "react-redux";
+
+const ChangeValueButton = styled.button`
+  display: inline-flex;
+
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  font-size: 36px;
+  border: 0;
+  border-radius: 50%;
+  width: 4rem;
+  height: 4rem;
+  margin: 1rem;
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 1rem;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span></span>
+        <ButtonWrapper>
+          <ChangeValueButton>+</ChangeValueButton>
+          <ChangeValueButton>-</ChangeValueButton>
+        </ButtonWrapper>
       </header>
     </div>
   );
